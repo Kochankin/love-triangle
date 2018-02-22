3efb9dbcf9001a0a507cfab8c305cc91bdaa9313
Kochanova Marianna <marian110791@mail.ru>
2018-02-22 21:53:56 +0300

    add a check

--- src/index.js
+++ src/index.js
@@ -4,26 +4,32 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   var count = 0;
-  
+    
   if (preferences.length < 3) {
     return 0;
   } else {
     for (var i = 0; i < preferences.length; i++) {
-      if (!getLoverForThird(preferences, i)) {
-        break;
-      }
-  
+       // if (getLoverForThird(preferences, i) ) {
+        //  break;
+      //  }
+    
       if (getLoverForThird(preferences, i) === (i + 1)) { 
         count++;   
       }
+      console.log(count);
     }
   }
-  
+    
   function getLoverForThird(preferences, i) {
     var firstLover = preferences[i];
     var secondLover = preferences[firstLover - 1];
     var thirdLover = preferences[secondLover - 1];
-    return thirdLover;
+  
+    if ((firstLover === secondLover) && (secondLover === thirdLover) && (firstLover === thirdLover)) {
+      return null;
+    } else {
+        return thirdLover;
+    }
   } 
   return count/3; 
 };

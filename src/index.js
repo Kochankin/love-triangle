efee20c13036c148e633d5ee78497423c5f437f1
Kochanova Marianna <marian110791@mail.ru>
2018-02-22 21:30:35 +0300

    function implementation

--- src/index.js
+++ src/index.js
@@ -3,5 +3,27 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  
+  if (preferences.length < 3) {
+    return 0;
+  } else {
+    for (var i = 0; i < preferences.length; i++) {
+      if (!getLoverForThird(preferences, i)) {
+        break;
+      }
+  
+      if (getLoverForThird(preferences, i) === (i + 1)) { 
+        count++;   
+      }
+    }
+  }
+  
+  function getLoverForThird(preferences, i) {
+    var firstLover = preferences[i];
+    var secondLover = preferences[firstLover - 1];
+    var thirdLover = preferences[secondLover - 1];
+    return thirdLover;
+  } 
+  return count/3; 
 };
